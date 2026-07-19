461932d13ec55d7e83be985211acebb32b686d0d
Teddy Pender <teddyjfpender@gmail.com>
2026-07-19 01:39:57 +0100

    Wire focused RISC-V proof tests

--- src/riscv_prover_test.zig
+++ src/riscv_prover_test.zig
@@ -11,6 +11,8 @@ pub const malicious_witness_tests = @import("tests/riscv/malicious_witness_test.
 pub const infra_trace = @import("frontends/riscv/infra_trace.zig");
 pub const interaction_gen = @import("frontends/riscv/air/interaction_gen.zig");
 pub const riscv_air_component = @import("frontends/riscv/air/component.zig");
+pub const hash_component = @import("frontends/riscv/air/memory_commitment/hash_component.zig");
+pub const proof_transcript = @import("frontends/riscv/proof_transcript.zig");
 
 test {
     _ = prover;
@@ -18,5 +20,7 @@ test {
     _ = malicious_witness_tests;
     _ = interaction_gen;
     _ = riscv_air_component;
+    _ = hash_component;
+    _ = proof_transcript;
     std.testing.refAllDeclsRecursive(infra_trace);
 }
